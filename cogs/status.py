42392789e3b59a65d8f9a74eb5c850876261079c
technicaldiff <80162515+technicaldiff@users.noreply.github.com>
2021-06-24 16:04:56 +0530

    Update status.py

--- cogs/status.py
+++ cogs/status.py
@@ -1,3 +1,4 @@
+import asyncio
 import requests
 from discord.ext import commands
 import discord
@@ -13,6 +14,7 @@ class Mcstatus(commands.Cog):
 
     @commands.command(pass_context=True)
     async def mcstatus(self, ctx, arg):
+        i = 0
         serverdata = arg
         data = requests.get(f"https://eu.mc-api.net/v3/server/ping/{serverdata}").json()
         motdto = requests.get(f"https://api.mcsrvstat.us/2/{serverdata}").json()
@@ -48,9 +50,48 @@ class Mcstatus(commands.Cog):
         try:
             embed.set_thumbnail(url=f"{data['favicon']}")
         except:
-            embed.set_thumbnail(value="Failed")
-        await ctx.send(embed=embed)
+            embed.set_thumbnail(url="https://media.minecraftforum.net/attachments/300/619/636977108000120237.png")
+        the_embed = await ctx.send(embed=embed)
+        while True:
+            # get new data
+            data = requests.get(f"https://eu.mc-api.net/v3/server/ping/{serverdata}").json()
+            motdto = requests.get(f"https://api.mcsrvstat.us/2/{serverdata}").json()
+            em = discord.Embed(title="Server status", colour=discord.Colour.blurple())
+            try:
+                em.add_field(name="Status", value=f"{motdto['online']} :green_circle:")
+            except:
+                em.add_field(name="Status", value="Offline :red_circle:")
+            try:
+                em.add_field(name="Players online", value=f"{data['players']['online']} / {data['players']['max']}")
+            except:
+                em.add_field(name="Players online", value="Failed")
+            try:
+                em.add_field(name="Latency", value=f"{data['took']} ms")
+            except:
+                em.add_field(name="Latency", value="Failed")
 
+            try:
+                em.add_field(name="Version", value=f"{data['version']['name']}")
+            except:
+                em.add_field(name="Version", value="Failed")
+
+            try:
+                em.add_field(name="Motd", value=f"{motdto['motd']['clean']}")
+            except:
+                em.add_field(name="Motd", value="Failed")
+
+            try:
+                em.add_field(name="Player Names", value=f"{motdto['players']['list']}")
+            except:
+                em.add_field(name="Player Names", value="Failed")
+
+            try:
+                em.set_thumbnail(url=f"{data['favicon']}")
+            except:
+                em.set_thumbnail(url="https://media.minecraftforum.net/attachments/300/619/636977108000120237.png")
+
+            await the_embed.edit(embed=em)
+            await asyncio.sleep(180)
 
 def setup(bot):
     bot.add_cog(Mcstatus(bot))
